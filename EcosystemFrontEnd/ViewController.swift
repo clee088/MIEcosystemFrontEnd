f6e3857aeb6636aa1ef3448e0aa8a10a4ca99e25
clee088 <42843045+clee088@users.noreply.github.com>
2019-11-11 18:16:16 -0500

    Initial Commit

---
+++ EcosystemFrontEnd/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EcosystemFrontEnd
+//
+//  Created by Christopher Lee on 11/11/19.
+//  Copyright © 2019 Christopher Lee. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
